f82dfa7a6784df550b7e8e50b656dd6a355ee21b
toniarvilommi <toni.arvilommi@gmail.com>
2021-09-23 11:17:08 +0300

    Update people.js

--- models/people.js
+++ models/people.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
+// Connect to mongodb and check promises
 mongoose.connect(url)
 	.then(result => {
 		console.log('connected to MongoDB')
@@ -10,12 +11,14 @@ mongoose.connect(url)
 		console.log('error connecting to MongoDB:', error.message)
 	})
 
+// New schema for mongodb
 const personSchema = new mongoose.Schema({
 	name: String,
 	number: String,
 
 })
 
+//Convert id
 personSchema.set('toJSON', {
 	transform: (document, returnedObject) => {
 		returnedObject.id = returnedObject._id.toString()
